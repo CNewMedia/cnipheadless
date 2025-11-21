1a7e074f1a05a32ef8de0810c69e0d66fb7a7527
CNewMedia <38378545+CNewMedia@users.noreply.github.com>
2025-11-21 22:24:44 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Lenis from "@studio-freight/lenis";
 
 export default function HomePage() {
   useEffect(() => {
